a32c191cc7ec7bc65af1810e705e0e59f3f3ec91
hajin2050 <fjrql90@naver.com>
2024-08-02 17:53:18 +0900

    running mlflow server in background

--- start_mlflow_server.sh
+++ start_mlflow_server.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000
+mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000 > mlflow_server.log 2>&1 &
